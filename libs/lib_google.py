e7b672468d3495b8ebd52229ba480619974512fa
kevinwulff <kevincwulff@gmail.com>
2022-08-06 22:59:52 -0700

    dfg

--- libs/lib_google.py
+++ libs/lib_google.py
@@ -3,9 +3,10 @@ from __future__ import print_function
 import datetime
 import os.path
 
-from google.auth.transport.requests import Request
+# from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+
+# from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
