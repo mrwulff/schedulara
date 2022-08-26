6795c4effe4e9c3f9cd1d756658dd56ab164529a
kevinwulff <kevincwulff@gmail.com>
2022-08-25 20:07:33 -0700

    lame

--- libs/lib_colors.py
+++
@@ -1,3 +0,0 @@
-
-
-colors=[‘Red’, ‘Pink’, ‘Purple’, ‘DeepPurple’, ‘Indigo’, ‘Blue’, ‘LightBlue’, ‘Cyan’, ‘Teal’, ‘Green’, ‘LightGreen’, ‘Lime’, ‘Yellow’, ‘Amber’, ‘Orange’, ‘DeepOrange’, ‘Brown’, ‘Gray’, ‘BlueGray’]
