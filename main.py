0987c4a1ac94b4e9b929bb6e41d13a1e79816211
mrwulff <kevincwulff@gmail.com>
2023-04-06 23:01:50 -0700

    sc4

--- main.py
+++ main.py
@@ -2157,7 +2157,7 @@ Demo: If you are new to our app or would like to see how it works, click this bu
                 #qq.name2.secondary_text='Schedulara'
         except:
             #qq.branding.background="images/wordart2.png"
-            qq.name2.text='Schedulara'
+            qq.name2.text='Schedulara4'
             print ('sch5')
         #if x['branding']== False:
         #    qq.name2.text="NOOOOOO"
