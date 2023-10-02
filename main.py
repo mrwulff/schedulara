2b3d2407f4c0ae283db18ff662a554f6fd63d9de
mrwulff <kevincwulff@gmail.com>
2023-10-02 12:18:45 -0700

    icons bug

--- main.py
+++ main.py
@@ -1947,7 +1947,7 @@ Demo: If you are new to our app or would like to see how it works, click this bu
         'L2','lightbulb',
         'L1','lightbulb',
         'SL','spotlight-beam',
-        'SUP','account-supervisor'
+        'SUP','account-group'
         'HL','lightbulb-multiple-outline',
         'F','forklift',
         'SH','hand-back-left',
