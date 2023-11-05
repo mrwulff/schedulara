adcacad90591dc5ad6b50a0406d621a34d19edcd
mrwulff <kevincwulff@gmail.com>
2023-11-04 21:15:24 -0700

    /

--- libs/lib_custom_views.py
+++ libs/lib_custom_views.py
@@ -1,9 +1,10 @@
 def webviews(App, ad, ios,self):
+
     from libs.webview import WebView
     #self.browser = WebView('https://www.google.com',
     #                           enable_javascript = True,
     #                           enable_downloads = True,
     #                           enable_zoom = True)
-    self.filename=ad+'realdata.html'
+    self.filename=ad+'/realdata.html'
     self.browser = WebView('file://'+self.filename)
     return ('wowzers')
