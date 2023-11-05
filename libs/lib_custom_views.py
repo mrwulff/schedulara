a9433ad4d948dee4162bdb258b30404f3ab17e3b
mrwulff <kevincwulff@gmail.com>
2023-11-04 20:55:36 -0700

    realfile

--- libs/lib_custom_views.py
+++ libs/lib_custom_views.py
@@ -1,7 +1,9 @@
 def webviews(App, ad, ios,self):
     from libs.webview import WebView
-    self.browser = WebView('https://www.google.com',
-                               enable_javascript = True,
-                               enable_downloads = True,
-                               enable_zoom = True)
+    #self.browser = WebView('https://www.google.com',
+    #                           enable_javascript = True,
+    #                           enable_downloads = True,
+    #                           enable_zoom = True)
+    self.filename=ad+'realdata.html'
+    self.browser = WebView('file://'self.filename)
     return ('wowzers')
